18fb026130b6b227a379300a6b81151c71b6da52
Brian Timar <timarbrian@gmail.com>
2019-05-05 11:14:55 -0700

    init description

---
+++ recommendation_display/__init__.py
@@ -0,0 +1,4 @@
+"""recommendation_display
+    App to replicate Tinder's basic functionality, with annotated images.
+    After providing credentials, user views photo collections from Tinder's recommendations, 
+    with faces common to photos highlighted. """
